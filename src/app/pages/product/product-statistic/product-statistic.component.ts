60a4c26e5124154d9641e10324ad7fa7e145d04d
wind <tuanphongmta0205@gmail.com>
2020-03-10 14:47:04 +0700

    done resize

--- src/app/pages/product/product-statistic/product-statistic.component.ts
+++ src/app/pages/product/product-statistic/product-statistic.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ResizeEvent } from 'angular-resizable-element';
+import { FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../../../core/services/api/product.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-statistic',
@@ -10,20 +15,41 @@ import { ResizeEvent } from 'angular-resizable-element';
   }
 })
 export class ProductStatisticComponent implements OnInit {
-  height: number = 200;
+  height: number = 300;
   containerHeight = 0;
-  constructor(public element: ElementRef) {
+  private destroyed$ = new Subject();
+  breadCrumbItems: Array<{}>;
+
+  submitted: boolean;
+  categories: any;
+
+  textSearch = '';
+  categorySearch = '';
+  page = 0;
+  pageSize = 10;
+  totalSize = 0;
+
+  selectedProduct = null;
+  products: any;
+  constructor(
+    public element: ElementRef,
+    public formBuilder: FormBuilder,
+    private productService: ProductService
+  ) {
     this.element.nativeElement;
   }
 
   ngOnInit() {
     var el = this.element.nativeElement;
     this.containerHeight = el.children[0].clientHeight;
+
+    this._fetchData();
+    this._fetchFilter();
   }
 
   onResizeEnd(event: ResizeEvent) {
     if (event) {
-      if (event.rectangle.height != null) {
+      if (event.rectangle.height != null && event.rectangle.height < 500) {
         this.height = event.rectangle.height;
       }
     }
@@ -35,7 +61,43 @@ export class ProductStatisticComponent implements OnInit {
   }
 
   onClickTest() {
-    if (this.height <= 10) this.height = 200;
+    if (this.height <= 10) this.height = 300;
     else this.height = 10;
+  }
+
+  onPageChange(page: number): void {
+    this.page = page - 1;
+    this._fetchData();
+  }
+
+  onChangeFilter() {
+    this.page--;
+    this._fetchData();
+  }
+
+  private _fetchData() {
+    const product$ = this.productService
+      .loadProduct({
+        pageNumber: this.page,
+        pageSize: this.pageSize,
+        search_name: this.textSearch,
+        category_id: this.categorySearch
+      })
+      .pipe(takeUntil(this.destroyed$));
+    product$.subscribe((res: any) => {
+      if (res) {
+        this.totalSize = res.Data.TotalNumberOfRecords;
+        this.products = res.Data.Results;
+        this.selectedProduct = this.products[0];
+      }
+    });
+  }
+
+  private _fetchFilter() {
+    const category$ = this.productService.loadCategory().pipe(takeUntil(this.destroyed$));
+
+    category$.subscribe((res: any) => {
+      this.categories = res.Data;
+    });
   }
 }
